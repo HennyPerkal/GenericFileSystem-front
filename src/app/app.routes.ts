4b19fc4437f7ae90812bbadfc921762871a1f943
rivkyneumann1223@gmail.com <rivkyneumann1223@gmail.com>
2024-11-24 10:44:24 +0200

    added component with styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ImportDataSourceComponent } from './components/import-data-source/import-data-source.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'add-import-data-source', component: ImportDataSourceComponent },
+  { path: '', redirectTo: '/add-import-data-source', pathMatch: 'full' },
+];
